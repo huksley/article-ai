88fb56497e0ae82255d28cb13f1565c0fd864e6d
Ruslan Gainutdinov <contact@ruslan.org>
2022-04-12 23:40:54 +0300

    Update

--- .platform/hooks/predeploy/download.sh
+++ .platform/hooks/predeploy/download.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 source /var/app/venv/*/bin/activate
 cd /var/app/staging
-bash ./download.sh
+./download.sh
 
+# This script is executed as root so move them where we can see them from webapp
+mv /root/nltk_data /var/app/venv/staging-LQM1lest/nltk_data
