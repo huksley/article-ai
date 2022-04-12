e11b8f3b184cb737dc5161e5ae08f2206dd46ec1
Ruslan Gainutdinov <contact@ruslan.org>
2022-04-12 21:03:37 +0300

    Move to root

--- app/__main__.py
+++
@@ -1,9 +0,0 @@
-"""Web app entry point, when running as python -m app"""
-from . import get_app
-
-app = get_app()
-
-if __name__ == "__main__":
-    app.jinja_env.auto_reload = True
-    app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run(host='0.0.0.0', port=8000)
