d84337e879634793ad724bfd58f0c1da578a7059
Ruslan Gainutdinov <contact@ruslan.org>
2022-04-12 10:35:17 +0300

    Add spacytextblob

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ import numpy as np
 import logging
 from json import loads
 import requests
-
+from spacytextblob.spacytextblob import SpacyTextBlob
 from flask import Flask, redirect, render_template, send_from_directory, request, url_for, session, Response
 
 
@@ -101,22 +101,17 @@ def get_models() -> List[str]:
 
 
 def todict(obj, classkey=None, level=0):
-    logger.info("Object %s", type(obj))
     if isinstance(obj, dict):
-        logger.info("isinstance dict")
         data = {}
         for (k, v) in obj.items():
             logger.info("Level items %s", k)
             data[k] = todict(v, classkey)
         return data
     elif hasattr(obj, "_ast"):
-        logger.info("_ast")
         return todict(obj._ast())
     elif hasattr(obj, "__iter__"):
-        logger.info("__iter__")
         return [todict(v, classkey) for v in obj]
     elif hasattr(obj, "__dict__"):
-        logger.info("__dict__")
         data = dict([(key, todict(value, classkey))
                      for key, value in obj.__dict__.iteritems()
                      if not callable(value) and not key.startswith('_') and key not in ['name']])
@@ -135,13 +130,16 @@ def todict(obj, classkey=None, level=0):
     elif hasattr(obj, "__unicode__"):
         return {'type': type(obj).__name__, 'text': obj.__unicode__()}
     else:
-        logger.info("Unknown asis")
+        logger.info("Unknown %s", type(obj))
         return {'type': type(obj).__name__}
 
 
 def get_data(doc: Doc) -> Dict[str, Any]:
-    """Extract the data to return from the REST API given a Doc object. Modify
-    this function to include other data."""
+    """
+    Extract the data to return from the REST API given a Doc object. Modify
+    this function to include other data.
+    See more here: https://spacy.io/usage/linguistic-features
+    """
     ents = []
     for ent in doc.ents:
         ents.append(
@@ -154,10 +152,8 @@ def get_data(doc: Doc) -> Dict[str, Any]:
     return {
         "text": doc.text,
         "ents": ents,
-        # "vector": doc.vector,
         "sentiment": doc.sentiment,
-        # "tensor": doc.tensor,
-        "score": doc._.text_id
+        "text_id": doc._.text_id
     }
 
 
@@ -168,18 +164,43 @@ class PythonObjectEncoder(json.JSONEncoder):
         return todict(obj)
 
 
-@app.get("/test")
-def test():
-    model = "en_core_web_sm"
+def process_texts(model, texts):
+    """
+    Process a batch of articles and return the entities predicted by the
+    given model. 
+    """
     if MODELS.get(model) is None:
         logger.info("Loading model %s", model)
-        MODELS[model] = spacy.load(model)
+        nlp = spacy.load(model)
+        nlp.add_pipe("spacytextblob")
+        MODELS[model] = nlp
 
-    """Process a batch of articles and return the entities predicted by the
-    given model. Each record in the data should have a key "text".
-    """
     nlp = MODELS[model]
-    response_body = []
+    response = []
+    docs = []
+    previous = None
+
+    for doc in nlp.pipe(texts):
+        docs.append(doc)
+
+    for doc in docs:
+        doc_data = get_data(doc)
+        if previous is not None:
+            doc_data["similarity"] = doc.similarity(previous)
+        if doc._.blob is not None:
+            doc_data["polarity"] = doc._.blob.polarity
+            doc_data["subjectivity"] = doc._.blob.subjectivity
+            doc_data["assessments"] = doc._.blob.sentiment_assessments.assessments
+            doc_data["ngrams"] = doc._.blob.ngrams()
+            doc_data["word_counts"] = doc._.blob.word_counts
+        response.append(doc_data)
+        previous = doc
+
+    return response
+
+
+@app.get("/test")
+def test():
     texts = [
         """
         A partnership between Finnish eco materials company Sulapac and Dubai-based sustainability startup BIRD Collaborative has seen millions of sustainable straws made from a patented wood chip and plant resin delivered to luxury hotels, restaurants, wellness centres and clubs in the United Arab Emirates since 2021.
@@ -204,18 +225,7 @@ UAE luxury hotel, The St. Regis Downtown Dubai, has taken sustainability to the 
 “We are delighted to partner with Bird Collaborative and move towards a more sustainable operation here at The St. Regis Downtown Dubai. We all need to focus on sustainability and simple solutions, like using alternatives to conventional plastic for everyday products, are an easy starting point,” says Raja Zeidan, General Manager at St. Regis Downtown Dubai.
         """
     ]
-    docs = []
-    for doc in nlp.pipe(texts):
-        docs.append(doc)
-    similarity = doc[0].similarity(doc[1])
-    for doc in docs:
-        response_body.append(
-            {
-                'similarity': similarity,
-                'doc': get_data(doc),
-                # 'raw': doc.to_json()
-            }
-        )
+    response_body = process_texts("en_core_web_sm", texts)
     resp = Response(json.dumps(response_body, cls=PythonObjectEncoder))
     resp.headers['Content-Type'] = 'application/json'
     return resp
@@ -224,26 +234,9 @@ UAE luxury hotel, The St. Regis Downtown Dubai, has taken sustainability to the 
 @app.post("/process")
 def process():
     data = request.get_json()
-    model = data.get("model") or "en_core_web_sm"
-    if MODELS.get(model) is None:
-        logger.info("Loading model %s", model)
-        MODELS[model] = spacy.load(model)
-
-    nlp = MODELS[model]
-    response_body = []
+    model = data.get("model") or "en_core_web_md"
     texts = data.get("texts") or []
-    docs = []
-    previous = None
-    for doc in nlp.pipe(texts):
-        docs.append(doc)
-
-    for doc in docs:
-        doc_data = get_data(doc)
-        if previous is not None:
-            doc_data["similarity"] = doc.similarity(previous)
-        response_body.append(doc_data)
-        previous = doc
-
+    response_body = process_texts(model, texts)
     resp = Response(json.dumps(response_body, cls=PythonObjectEncoder))
     resp.headers['Content-Type'] = 'application/json'
     return resp
